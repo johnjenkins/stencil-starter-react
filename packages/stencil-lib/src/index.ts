03e23879832672bc9f5b9f645e7575290ef9ed8c
John Jenkins <johnljenkins@Hotmail.com>
2026-01-21 15:40:42 +0000

    index.ts setup

--- packages/stencil-lib/src/index.ts
+++ packages/stencil-lib/src/index.ts
@@ -1,1 +1,7 @@
-// setup utils
+import { setMode } from "@stencil/core";
+
+export function initialize() {
+  setMode((el) => {
+    return el.getAttribute("mode") || (el as any).mode || "light";
+  });
+}
